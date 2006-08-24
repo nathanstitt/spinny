18f295a8d02ca22f92e7540ee3a12439a70ceee0
nas <nas@9787d06c-1a0a-0410-a88a-902e1a4f81df>
2006-08-24 20:26:57 +0000

    git-svn-id: file:///srv/svn/spinny@302 9787d06c-1a0a-0410-a88a-902e1a4f81df

--- vendor/unit_test/test.h
+++ vendor/unit_test/test.h
@@ -10,4 +10,34 @@
 #include "CheckMacros.h"
 #include "TestRunner.h"
 
+#include "TestDetails.h"
+
+namespace UnitTest {
+
+class TestResults;
+class TestList;
+
+class Test
+{
+public:
+    Test(char const* testName, char const* suiteName = "DefaultSuite", char const* filename = "", int lineNumber = 0);
+    virtual ~Test();
+    void Run(TestResults& testResults) const;
+
+    TestDetails const m_details;
+    Test* next;
+    mutable bool m_timeConstraintExempt;
+
+    static TestList& GetTestList();
+
+private:
+    virtual void RunImpl(TestResults& testResults_) const;
+
+    Test(Test const&);
+    Test& operator =(Test const&);
+};
+
+
+}
+
 #endif
