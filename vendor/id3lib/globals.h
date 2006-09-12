fd437491ef272fd8be579ad5bfd120725cd0823d
nas <nas@9787d06c-1a0a-0410-a88a-902e1a4f81df>
2006-09-12 01:07:25 +0000

    set to gcc unused
    git-svn-id: file:///srv/svn/spinny@311 9787d06c-1a0a-0410-a88a-902e1a4f81df

--- vendor/id3lib/globals.h
+++ vendor/id3lib/globals.h
@@ -510,7 +510,11 @@ ID3_STRUCT(Mp3_Headerinfo)
 };
 
 #define ID3_NR_OF_V1_GENRES 148
-static const char *ID3_v1_genre_description[ID3_NR_OF_V1_GENRES] =
+static const char 
+#ifdef __GNUC__
+__attribute__((unused))
+#endif // UNIX
+*ID3_v1_genre_description[ID3_NR_OF_V1_GENRES] =
 {
   "Blues",             //0
   "Classic Rock",      //1
